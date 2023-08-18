d63612d9b940a64937291a0bf2b9089e4cd4ffc8
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-08-18 20:13:52 -0300

    validate SingUp

--- src/Views/SingUp/validation.js
+++ src/Views/SingUp/validation.js
@@ -1,6 +1,10 @@
 const validate = (userData) => {
    const errors={}; 
 
+   if (!userData.name || !/^(?!^\s*$)[A-Za-z0-9\s]{3,25}$/.test(userData.name) || userData.name.length < 3 || userData.name.length >= 25) {
+    errors.name = 'El nombre debe contener de 3 a 25 caracteres, solo letras y números';
+  }
+
     if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(userData.email)){
         errors.email='el email ingresado no es valido'
     }
